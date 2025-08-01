fc47d0b5e33176122df23a5a4b7e0c19d182f901
Mikko <62485576+miberr@users.noreply.github.com>
2025-08-01 23:06:22 +0300

    Next section

---
+++ 15-redux-intro/redux-intro/src/Store.js
@@ -0,0 +1,10 @@
+import customerReducer from './features/customers/customerSlice';
+import accountReducer from './features/accounts/accountSlice';
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: { account: accountReducer, customer: customerReducer },
+});
+
+export default store;
---
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: '',
+    isLoading: false,
+};
+
+const accountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {
+        deposit(state, action) {
+            state.balance += action.payload;
+            state.isLoading = false;
+        },
+        withdraw(state, action) {
+            state.balance -= action.payload;
+        },
+        requestLoan: {
+            prepare(amount, purpose) {
+                return { payload: { amount, purpose } };
+            },
+            reducer(state, action) {
+                if (state.loan > 0) return;
+
+                state.loan = action.payload.amount;
+                state.loanPurpose = action.payload.purpose;
+                state.balance += action.payload.amount;
+            },
+        },
+        payLoan(state) {
+            state.loanPurpose = '';
+            state.balance -= state.loan;
+            state.loan = 0;
+        },
+        convertingCurrency(state) {
+            state.isLoading = true;
+        },
+    },
+});
+
+export function deposit(amount, currency) {
+    if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+    return async function (dispatch, getState) {
+        dispatch({ type: 'account/convertingCurrency' });
+        // API call
+        const res = await fetch(
+            `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`
+        );
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        return dispatch({ type: 'account/deposit', payload: converted });
+    };
+}
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
